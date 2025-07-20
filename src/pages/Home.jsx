269ab88e9201c010e3beac498308ceac6a27f546
Yashpunjbi <128663439+Yashpunjbi@users.noreply.github.com>
2025-07-20 23:59:59 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,1 +1,28 @@
+import React from "react";
+
+const menuItems = [
+  {
+    id: 1,
+    name: "Cheesy Pizza",
+    price: 29,
+    image: "https://source.unsplash.com/400x300/?pizza",
+  },
+  {
+    id: 2,
+    name: "Masala Sandwich",
+    price: 29,
+    image: "https://source.unsplash.com/400x300/?sandwich",
+  },
+  {
+    id: 3,
+    name: "Cold Drink",
+    price: 9,
+    image: "https://source.unsplash.com/400x300/?cold-drink",
+  },
+];
+
+const Home = () => {
+  return (
+    <div className="p-4 mb-16">
+      <h2 className="text-xl font-bold mb-4 text-center text-pink-600
 
