c2fd6e1d0b81f5f11d25f1ed26c257dcaef9ee65
Yashpunjbi <128663439+Yashpunjbi@users.noreply.github.com>
2025-07-21 00:01:24 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,1 +1,10 @@
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+
+const Navbar = () => {
+  const { pathname } = useLocation();
+
+  const navItems = [
+    { name: "Home", path: "/", icon: "🏠" },
+    { name
 
