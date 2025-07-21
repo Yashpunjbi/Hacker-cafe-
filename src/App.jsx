33d54f9eb256138aea286bb260522d69988e8a3c
Yashpunjbi <128663439+Yashpunjbi@users.noreply.github.com>
2025-07-22 01:38:48 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,25 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Offers from "./pages/Offers";
 import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout"; // agar ye bana liya hai
 import Navbar from "./components/Navbar";
+import { CartProvider } from "./context/CartContext"; // 👈 import context
 
 const App = () => {
   return (
-    <Router>
-      <div className="min-h-screen bg-gray-50 pb-20">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/offers" element={<Offers />} />
-          <Route path="/cart" element={<Cart />} />
-        </Routes>
-        <Navbar />
-      </div>
-    </Router>
+    <CartProvider>
+      <Router>
+        <div className="min-h-screen bg-gray-50 pb-20">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/offers" element={<Offers />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/checkout" element={<Checkout />} /> {/* Optional */}
+          </Routes>
+          <Navbar />
+        </div>
+      </Router>
+    </CartProvider>
   );
 };
 
