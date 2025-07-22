79169dbb51b4ff06f869c991e3cf26dbf85cc043
Yashpunjbi <128663439+Yashpunjbi@users.noreply.github.com>
2025-07-22 14:00:50 +0530

    Update BottomNav.jsx

--- src/components/BottomNav.jsx
+++ src/components/BottomNav.jsx
@@ -1,21 +1,23 @@
 import { FaHome, FaTags, FaShoppingCart } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const BottomNav = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="fixed bottom-0 left-0 right-0 bg-white border-t shadow-md flex justify-around p-2 z-50">
-      <Link to="/" className="flex flex-col items-center text-pink-600">
+      <button onClick={() => navigate("/")} className="flex flex-col items-center text-pink-600">
         <FaHome size={20} />
         <span className="text-xs">Home</span>
-      </Link>
-      <Link to="/" className="flex flex-col items-center text-pink-600">
+      </button>
+      <button onClick={() => navigate("/offers")} className="flex flex-col items-center text-pink-600">
         <FaTags size={20} />
         <span className="text-xs">Offers</span>
-      </Link>
-      <Link to="/cart" className="flex flex-col items-center text-pink-600">
+      </button>
+      <button onClick={() => navigate("/cart")} className="flex flex-col items-center text-pink-600">
         <FaShoppingCart size={20} />
         <span className="text-xs">Cart</span>
-      </Link>
+      </button>
     </div>
   );
 };
