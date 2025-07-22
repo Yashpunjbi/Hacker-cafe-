2baea6966dbcd511e9c8932640b3c7f64851973c
Yashpunjbi <128663439+Yashpunjbi@users.noreply.github.com>
2025-07-22 13:16:45 +0530

    Update BottomNav.jsx

--- src/components/BottomNav.jsx
+++ src/components/BottomNav.jsx
@@ -1,21 +1,21 @@
-// src/components/BottomNav.jsx
 import { FaHome, FaTags, FaShoppingCart } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const BottomNav = () => {
   return (
     <div className="fixed bottom-0 left-0 right-0 bg-white border-t shadow-md flex justify-around p-2 z-50">
-      <button className="flex flex-col items-center text-pink-600">
+      <Link to="/" className="flex flex-col items-center text-pink-600">
         <FaHome size={20} />
         <span className="text-xs">Home</span>
-      </button>
-      <button className="flex flex-col items-center text-pink-600">
+      </Link>
+      <Link to="/" className="flex flex-col items-center text-pink-600">
         <FaTags size={20} />
         <span className="text-xs">Offers</span>
-      </button>
-      <button className="flex flex-col items-center text-pink-600">
+      </Link>
+      <Link to="/cart" className="flex flex-col items-center text-pink-600">
         <FaShoppingCart size={20} />
         <span className="text-xs">Cart</span>
-      </button>
+      </Link>
     </div>
   );
 };
