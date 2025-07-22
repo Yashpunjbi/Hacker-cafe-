9b837366540b3e041c5f4e7a0401de23ad0286d0
Yashpunjbi <128663439+Yashpunjbi@users.noreply.github.com>
2025-07-22 12:52:31 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,21 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Offers from "./pages/Offers";
+import { CartProvider } from "./context/CartContext";
+import Menu from "./pages/Menu";
 import Cart from "./pages/Cart";
-import Checkout from "./pages/Checkout"; // ✅ Checkout page
-import Navbar from "./components/Navbar";
-import { CartProvider } from "./context/CartContext"; // ✅ Cart context
+import Checkout from "./pages/Checkout";
 
 const App = () => {
   return (
-    <Router>
-      <CartProvider>
-        <div className="min-h-screen bg-gray-50 pb-20">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/offers" element={<Offers />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/checkout" element={<Checkout />} />
-          </Routes>
-          <Navbar />
-        </div>
-      </CartProvider>
-    </Router>
+    <CartProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Menu />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout" element={<Checkout />} />
+        </Routes>
+      </Router>
+    </CartProvider>
   );
 };
 
