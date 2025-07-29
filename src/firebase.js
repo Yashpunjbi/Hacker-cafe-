4c88b66bb96c7b818c84b80281bfdeda8592d81e
Yashpunjbi <128663439+Yashpunjbi@users.noreply.github.com>
2025-07-29 16:43:23 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,24 +1,21 @@
 // src/firebase.js
-
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore"; // ✅ Firestore import
-import { getAnalytics } from "firebase/analytics";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyC00vdY1ePvGb6eIq3GYTn9sZgHQm0lqGc",
   authDomain: "ddoskitchen.firebaseapp.com",
   projectId: "ddoskitchen",
-  storageBucket: "ddoskitchen.firebasestorage.app",
+  storageBucket: "ddoskitchen.firebaseapp.com",
   messagingSenderId: "57929834946",
   appId: "1:57929834946:web:3975dcb77bbe7bb1d5765b",
-  measurementId: "G-V49H346Q22"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
-// ✅ Initialize Firestore
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
 const db = getFirestore(app);
 
-export { db }; // ✅ Export this so Checkout.jsx can use it
+export { auth, provider, db };
