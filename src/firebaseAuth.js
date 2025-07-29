67fb8f6bec704600ec122852bc3721ff1de31988
Yashpunjbi <128663439+Yashpunjbi@users.noreply.github.com>
2025-07-29 15:37:11 +0530

    Update firebaseAuth.js

--- src/firebaseAuth.js
+++ src/firebaseAuth.js
@@ -1,20 +1,19 @@
-import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut } from "firebase/auth";
-import { app } from "./firebase"; // yeh tere existing firebase config se aa raha
+// src/firebase/FirebaseAuth.js
+import { initializeApp } from "firebase/app";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyC00vdY1ePvGb6eIq3GYTn9sZgHQm0lqGc",
+  authDomain: "ddoskitchen.firebaseapp.com",
+  projectId: "ddoskitchen",
+  storageBucket: "ddoskitchen.firebasestorage.app",
+  messagingSenderId: "57929834946",
+  appId: "1:57929834946:web:3975dcb77bbe7bb1d5765b",
+  measurementId: "G-V49H346Q22"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
-export const loginUser = (email, password) => {
-  return signInWithEmailAndPassword(auth, email, password);
-};
-
-export const registerUser = (email, password) => {
-  return createUserWithEmailAndPassword(auth, password);
-};
-
-export const logoutUser = () => {
-  return signOut(auth);
-};
-
-export const getCurrentUser = () => {
-  return auth.currentUser;
-};
+export { auth, createUserWithEmailAndPassword, signInWithEmailAndPassword };
