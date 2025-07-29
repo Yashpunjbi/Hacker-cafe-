e1b33b2c86a5f6253490185c22823ec6cb2df024
Yashpunjbi <128663439+Yashpunjbi@users.noreply.github.com>
2025-07-29 13:09:05 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { CartProvider } from "./context/CartContext";
 import Menu from "./pages/Menu";
 import Cart from "./pages/Cart";
-import Offers from "./components/Offers";
+import Offers from "./pages/Offers";
 import Checkout from "./pages/Checkout";
 import BottomNav from "./components/BottomNav";
 
