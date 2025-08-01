81dd4a8c44932ab30a7dae366ef8390629b28c75
Yashpunjbi <128663439+Yashpunjbi@users.noreply.github.com>
2025-08-01 16:24:12 +0530

    Update BottomNav.jsx

--- src/components/BottomNav.jsx
+++ src/components/BottomNav.jsx
@@ -6,26 +6,45 @@ const BottomNavbar = () => {
   const location = useLocation();
 
   const navItems = [
-    { name: 'Home', path: '/', icon: <FaHome /> },
-    { name: 'Offers', path: '/offers', icon: <FaTags /> },
-    { name: 'Cart', path: '/cart', icon: <FaShoppingCart /> },
-    { name: 'Profile', path: '/profile', icon: <FaUser /> },
+    {
+      name: 'Home',
+      path: '/',
+      icon: <FaHome size={22} />,
+    },
+    {
+      name: 'Offers',
+      path: '/offers',
+      icon: <FaTags size={22} />,
+    },
+    {
+      name: 'Cart',
+      path: '/cart',
+      icon: <FaShoppingCart size={22} />,
+    },
+    {
+      name: 'Profile',
+      path: '/profile',
+      icon: <FaUser size={22} />,
+    },
   ];
 
   return (
-    <div className="fixed bottom-0 w-full h-[60px] bg-white shadow-md flex justify-around items-center border-t rounded-t-2xl z-50">
-      {navItems.map((item) => (
-        <Link
-          key={item.name}
-          to={item.path}
-          className={`flex flex-col items-center text-sm ${
-            location.pathname === item.path ? 'text-orange-500' : 'text-gray-500'
-          }`}
-        >
-          <div className="text-xl">{item.icon}</div>
-          <span className="text-[10px]">{item.name}</span>
-        </Link>
-      ))}
+    <div className="fixed bottom-0 w-full h-[60px] bg-white shadow-lg border-t border-gray-200 flex justify-around items-center z-50">
+      {navItems.map((item) => {
+        const isActive = location.pathname === item.path;
+        return (
+          <Link
+            to={item.path}
+            key={item.name}
+            className={`flex flex-col items-center text-xs font-medium ${
+              isActive ? 'text-orange-500' : 'text-gray-500'
+            }`}
+          >
+            <div>{item.icon}</div>
+            <span>{item.name}</span>
+          </Link>
+        );
+      })}
     </div>
   );
 };
