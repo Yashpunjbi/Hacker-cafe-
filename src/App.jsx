e66908acbfe0dbcde3c0b9420a5ee20846bfb992
Yashpunjbi <128663439+Yashpunjbi@users.noreply.github.com>
2025-08-06 15:37:23 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { CartProvider } from "./context/CartContext";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./firebase";
+
 import Home from "./pages/Home";
 import Menu from "./pages/Menu";
 import Cart from "./pages/Cart";
@@ -11,16 +14,28 @@ import Login from "./pages/Login";
 import Track from "./pages/Track";
 import Contact from "./pages/Contact";
 import Terms from "./pages/Terms";
+
 import BottomNavbar from "./components/BottomNav";
 import Navbar from "./components/Navbar";
+
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currUser) => {
+      setUser(currUser);
+    });
+
+    return () => unsubscribe(); // cleanup
+  }, []);
+
   return (
     <CartProvider>
       <Router>
-        <Navbar />
+        <Navbar user={user} />
         <div className="pb-20 pt-16">
           <Routes>
             <Route path="/" element={<Home />} />
